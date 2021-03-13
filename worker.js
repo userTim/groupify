3bcf153c09a06d8f13f0dd39a21a53eeafec92ac
userTim <usertim1205@gmail.com>
2021-03-13 21:15:01 +0900

    Added sub items to context menu

--- worker.js
+++ worker.js
@@ -1,18 +1,39 @@
-function installedHandler() {
-    console.log('test')
+const DOCUMENT_URL_PATTERNS = ['http://*/*', 'https://*/*']
+const MAIN_ITEM_ID = 'groupify_main'
 
-    chrome.contextMenus.create(
-        {
-            title: 'KEKW',
-            id: 'groupify13',
-            documentUrlPatterns: ['http://*/*', 'https://*/*'],
-        },
-        () => console.log('Context menu item created')
-    )
+async function installedHandler() {
+    await createContextMenuMainItem()
 
     chrome.contextMenus.onClicked.addListener((info, tab) => {
         console.log('Item ' + info.menuItemId + ' clicked ' + 'in tab ' + tab.url)
     })
+}
+
+function createContextMenuMainItem() {
+    return new Promise((resolve, reject) =>
+        chrome.contextMenus.create(
+            {
+                id: MAIN_ITEM_ID,
+                title: 'KEKW',
+                documentUrlPatterns: DOCUMENT_URL_PATTERNS,
+            },
+            () => (chrome.runtime.lastError ? reject(Error(chrome.runtime.lastError.message)) : resolve())
+        )
+    )
+}
+
+function createContextMenuSubItem(title, id) {
+    return new Promise((resolve, reject) =>
+        chrome.contextMenus.create(
+            {
+                title,
+                id: `groupify_${id}`,
+                parentId: MAIN_ITEM_ID,
+                documentUrlPatterns: DOCUMENT_URL_PATTERNS,
+            },
+            () => (chrome.runtime.lastError ? reject(Error(chrome.runtime.lastError.message)) : resolve())
+        )
+    )
 }
 
 chrome.runtime.onInstalled.addListener(installedHandler)
