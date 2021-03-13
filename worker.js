497a8ce622933ea3f573ae53cac1c5a93f5b6d79
userTim <usertim1205@gmail.com>
2021-03-13 19:38:26 +0900

    Added context menu

--- worker.js
+++ worker.js
@@ -1,3 +1,18 @@
-chrome.runtime.onInstalled.addListener(() => {
+function installedHandler() {
     console.log('test')
-})
+
+    chrome.contextMenus.create(
+        {
+            title: 'KEKW',
+            id: 'groupify13',
+            documentUrlPatterns: ['http://*/*', 'https://*/*'],
+        },
+        () => console.log('Context menu item created')
+    )
+
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+        console.log('Item ' + info.menuItemId + ' clicked ' + 'in tab ' + tab.url)
+    })
+}
+
+chrome.runtime.onInstalled.addListener(installedHandler)
