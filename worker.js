4f32ba1672a77881e82c142450940654ed9398aa
userTim <usertim1205@gmail.com>
2021-03-13 21:17:24 +0900

    Promisified interaction /w storage

--- worker.js
+++ worker.js
@@ -36,4 +36,20 @@ function createContextMenuSubItem(title, id) {
     )
 }
 
+function getStorageData(key) {
+    return new Promise((resolve, reject) =>
+        chrome.storage.sync.get(key, (result) =>
+            chrome.runtime.lastError ? reject(Error(chrome.runtime.lastError.message)) : resolve(result)
+        )
+    )
+}
+
+function setStorageData(data) {
+    return new Promise((resolve, reject) =>
+        chrome.storage.sync.set(data, () =>
+            chrome.runtime.lastError ? reject(Error(chrome.runtime.lastError.message)) : resolve()
+        )
+    )
+}
+
 chrome.runtime.onInstalled.addListener(installedHandler)
